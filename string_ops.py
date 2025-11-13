ae68b2de202e265dbb10ea596718337f67098fb2
Your Name <you@example.com>
2025-11-13 22:28:10 +0200

    finish the project

--- string_ops.py
+++ string_ops.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-# from pydantic import BaseModel
+from classitem import *
 # import uvicorn
 import json
 app = FastAPI()
@@ -16,11 +16,35 @@ def reverse(text:str):
 def to_upper(text: str):
     return {"original":text,"uppercased":text.upper()}
 
-def remove_vowels(s: str) -> str:
-    pass
+@app.post("/remove-vowels")
+def remove_vowels(item:Item) -> str:
+        str1 = ""
+        for s in item.text:
+            if s not in ["a","e","i","o","u"]:
+                str1+=s
+        dict = str({ "original":item.text, "without_vowels":str1 })
+        return dict
 
-def remove_every_third(s: str) -> tuple[str, list[int], list[str]]:
-    pass
+@app.post("/remove-every-third")
+def remove_every_third(item:Item) ->dict:
+    str1 = ""
+    for i in range(len(item.text)):
+        if (i+1) % 3 != 0:
+            str1+=item.text[i]
+    return { "original":item.text,"result": str1}
 
-def letter_counts_map(s: str) -> dict[str, int]:
-    pass
+@app.post("/letter-counts")
+def letter_counts_map(item:Item) -> dict:
+    dict = {}
+    list = []
+    for s in item.text:
+        if s not in list:
+            list.append(s)
+            dict[s] = 1
+        else:
+            dict[s]+=1
+    with open('data/letter_counts.json','w') as f:
+        dict1 = json.dumps(dict)
+        f.write(dict1)
+    return {"original":item.text,"counts":dict,"save to":"data/letter_counts.json"}
+
